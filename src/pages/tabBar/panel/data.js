353103b843929cce2c03a8926730bb81b0b2a419
sunzheng <haihai211>
2020-06-28 19:57:39 +0800

    基本样式

---
+++ src/pages/tabBar/panel/data.js
@@ -0,0 +1,95 @@
+export const GRIDS = [
+  {
+    id: 0,
+    title: "记账",
+    list: [
+      {
+        id: 0,
+        url: "/static/c1.png",
+        text: "花销",
+        desc: "",
+        badge: "0",
+      },
+      {
+        id: 1,
+        url: "/static/c2.png",
+        text: "工资",
+        desc: "",
+        badge: "1",
+      },
+      {
+        id: 2,
+        url: "/static/c3.png",
+        text: "股市",
+        desc: "",
+        badge: "99",
+      },
+      {
+        id: 3,
+        url: "/static/c4.png",
+        text: "外快",
+        desc: "",
+        badge: "2",
+      },
+      {
+        id: 4,
+        url: "/static/c4.png",
+        text: "总资产",
+        desc: "",
+        badge: "2",
+      },
+    ],
+  },
+  {
+    id: 1,
+    title: "备忘",
+    list: [
+      {
+        id: 0,
+        url: "/static/c1.png",
+        text: "身份信息",
+        badge: "0",
+      },
+      {
+        id: 1,
+        url: "/static/c1.png",
+        text: "金融",
+        desc: "银行卡 | 信用卡 | 医保卡",
+        badge: "0",
+      },
+      {
+        id: 2,
+        url: "/static/c1.png",
+        text: "互联网",
+        desc: "网站 | app",
+        badge: "0",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "习惯",
+    list: [
+      {
+        id: 0,
+        url: "/static/c1.png",
+        text: "作息时间",
+        badge: "0",
+      },
+      {
+        id: 1,
+        url: "/static/c1.png",
+        text: "喝饮料",
+        desc: "类型 | 量 | 频率",
+        badge: "0",
+      },
+      {
+        id: 2,
+        url: "/static/c1.png",
+        text: "锻炼",
+        desc: "时长 | 类型 | 频率",
+        badge: "0",
+      },
+    ],
+  },
+];
---
+++ src/pages/tabBar/panel/panel.vue
@@ -0,0 +1,91 @@
+<template>
+  <view class="panel-page">
+    <view v-for="grid in GRIDS" :key="grid.id">
+      <uni-section :title="grid.title" type="line" ></uni-section>
+      <view class="example-body">
+        <uni-grid :column="3" :highlight="true" @change="change" :show-border="true" :square="true">
+          <uni-grid-item v-for="(item, index) in grid.list" :index="index" :key="item.id">
+            <view class="grid-item-box">
+              <text class="text">{{ item.text }}</text>
+              <text class="desc">{{ item.desc }}</text>
+            </view>
+          </uni-grid-item>
+        </uni-grid>
+      </view>
+    </view>
+  </view>
+</template>
+
+<script>
+import { GRIDS } from "./data";
+export default {
+  data() {
+    return {
+      GRIDS
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* 头条小程序组件内不能引入字体 */
+/* #ifdef MP-TOUTIAO */
+/* @font-face {
+		font-family: uniicons;
+		font-weight: normal;
+		font-style: normal;
+		src: url('~@/static/uni.ttf') format('truetype');
+	} */
+
+/* #endif */
+
+page {
+  display: flex;
+  flex-direction: column;
+  box-sizing: border-box;
+  min-height: 100%;
+  height: auto;
+}
+
+view {
+  font-size: 14px;
+  line-height: inherit;
+}
+.example-body {
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  padding: 15px;
+  font-size: 14px;
+  background-color: #ffffff;
+}
+
+
+
+.image {
+  width: 50rpx;
+  height: 50rpx;
+}
+
+.text {
+  font-size: 16px;
+  font-weight: bold;
+  letter-spacing: 1.2px;
+  margin-top: 66rpx;
+}
+.desc {
+  font-size: 12px;
+  line-height: 1.3;
+  color: #cccccc;
+  text-align: center;
+}
+
+.grid-item-box {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 15rpx 0;
+  overflow: hidden;
+}
+</style>
